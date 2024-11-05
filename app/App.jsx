ef00d91daf73589540f4c057ab44e98ec53de9ca
karenLS3 <karen.lima.sanchez@gmail.com>
2024-11-04 21:23:08 -0500

    starting react native setup

---
+++ app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  SafeAreaView,
+} from 'react-native'
+import SplashScreen from './src/screens/Splash';
+
+function App(){
+
+  return (
+    <SplashScreen/>
+  );
+}
+
+export default App
